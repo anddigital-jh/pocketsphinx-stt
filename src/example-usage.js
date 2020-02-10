45e882296e2a6d2f36e977dc47725fb3d37db7ff
Jabari Holder <jholder@and.digital>
2020-02-10 17:17:50 +0000

    Update example-usage.js

--- src/example-usage.js
+++ src/example-usage.js
@@ -1,14 +1,18 @@
 const convertAndTranscribe = require('./index.js').convertAndTranscribe;
 const transcribe = require('./index.js').transcribe;
 
-const filePath = process.argv[2];
+console.log(process.argv[1]);
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+const filePath = '../test.mp3';
 
 // transcribe(filePath)
 //     .then((res) => {
 //     console.log('transcribe', res);
 //     })
 
-const videoFilePath = process.argv[2];
+const videoFilePath = filePath;
 convertAndTranscribe(videoFilePath)
     .then((res) => {
     console.log('transcribe', res);
